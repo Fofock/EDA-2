639c695b4ee8262c8bdd803194eb55547a04d692
Fofuck <adolfo.eme05@gmail.com>
2024-09-22 10:41:16 -0600

    Inicio Practica 6

---
+++ Practica6.py
@@ -0,0 +1,7 @@
+"Una clase es una plantilla que nos sirve para crear objetos, los cuales son un tipo de dato"
+#TODOS LOS OBJETOS TIENEN METODOS YA ESTABLECIDOS POR QUE TODOS LOS OBJETOS HEREDAN DE UNA SUPERCLASE
+#EL METODO REPR NOS AYUDA A QUE PODAMOS REGRESAR UNA CADENA CUANDO LLAMAMOS A ESTA FUNCION
+#UNA ARISTA SE DEFINE COMO DOS VERTICES (ARISTA AB)
+class gordas():
+    def __init__(self, nombt) -> None:
+        
