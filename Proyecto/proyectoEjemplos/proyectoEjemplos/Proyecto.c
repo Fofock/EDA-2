e8b971e3202347c818f5b9e56f51df5000294a4a
Fofuck <adolfo.eme05@gmail.com>
2024-11-30 11:13:46 -0600

    Working succesfully paralel code

--- Proyecto/proyectoEjemplos/proyectoEjemplos/Proyecto.c
+++ Proyecto/proyectoEjemplos/proyectoEjemplos/Proyecto.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb-master/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb-master/stb_image_write.h"
 
-void splitChannels();
-void CountingSort(unsigned char A[], int width, int height, int channels);
+double splitChannels();
+double CountingSort(unsigned char A[], int width, int height, int channels);
 void Ecualizacion(int C[], int width, int height);
 void GuardarHistograma(int C[], int B[]);
+double splitChannelsParalel(char *Img);
+double CountingSortParalel(unsigned char A[], int width, int height, int channels);
 
 int main(int argc, char *argv[])
 {
@@ -23,11 +26,14 @@ int main(int argc, char *argv[])
     }
     // EL ARGUMENTO 0 DE UN PROGRAMA ES EL PROPIO NOMBRE DEL EJECUTABLE, POR ESO NOS VAMOS AL INDICE UNO DE ARGV
     char *Img = argv[1]; 
-    splitChannels(Img);
+    double timeEjecParalela = splitChannelsParalel(Img);
+    double timeEjecSecuencial = splitChannels(Img);
+    printf("%lfs\n",timeEjecParalela);
+    printf("%lfs\n", timeEjecSecuencial);
     return 0;
 }
 
-void splitChannels(char *Img)
+double splitChannels(char *Img)
 {
     // OBTENERMOS EL VALOR QUE ALOGA A DONDE APUNTA IMG
     char *srcPath = Img; 
@@ -39,7 +45,7 @@ void splitChannels(char *Img)
     if (srcIma == NULL)
     {
         printf("No se pudo cargar la imagen %s :(\n\n\n", srcPath);
-        return;
+        return -1;
     }
     else if(channels == 3)
     {
@@ -72,7 +78,7 @@ void splitChannels(char *Img)
         }
 
         // CON EL ARREGLO IMARED YA PODEMOS OBTENER EL HISTOGRAMA APOYANDONOS DEL ALGORITMO COUNTING SORT
-        CountingSort(imaRed, width, height, channels);
+        double tEjecucionSecuencial = CountingSort(imaRed, width, height, channels);
         // DESPUES DEL COUNTING SORT IMARED (EL ARREGLO), YA ESTA ECUALIZADO
 
 
@@ -85,30 +91,32 @@ void splitChannels(char *Img)
         stbi_image_free(imaBlue);  // free memory
         stbi_image_free(imaRed);   // free memory
         stbi_image_free(imaGreen); // free memory
-        return;
+        return tEjecucionSecuencial;
     }
     else if (channels == 1)
     {
         printf("\nImagen cargada correctamente: %dx%d pixeles con %d canales.\n", width, height, channels);
 
         // CON EL ARREGLO IMARED YA PODEMOS OBTENER EL HISTOGRAMA APOYANDONOS DEL ALGORITMO COUNTING SORT
-        CountingSort(srcIma, width, height, channels);
+        double tEjecucionSecuencial = CountingSort(srcIma, width, height, channels);
         // DESPUES DEL COUNTING SORT IMARED (EL ARREGLO), YA ESTA ECUALIZADO
         // Saving image
         stbi_write_jpg("eqIma.jpg", width, height, 1, srcIma, 100);
 
         // Liberar la memoria de la imágen
         stbi_image_free(srcIma);  // free memory
+        return tEjecucionSecuencial;
     }
     else{
         printf("\nERROR: La imagen debe de ser de 1 0 3 canales.");
+        return -1;
     }
 
 
 }
 
 // A LA FUNCION LE PASAMOS EL ARREGLO, RECORDANDO QUE EN C LOS ARREGLOS SIEMPRE SE PASAN POR REFERENCA, ES DECIR EL PUNTERO QUE APUNTA A ESTOS 
-void CountingSort(unsigned char A[], int width, int height, int channels) {
+double CountingSort(unsigned char A[], int width, int height, int channels) {
     // BUSQUEDA DEL MAXIMO, EN ESTE CASO NO ES NECESARIO POR QUE NUESTRSO ARREGLOS SIEMPRE SERAN DE TAMAÑO 256 POR QUE SON LOS VALORES QUE PUEDEN TOMAR LOS ELEMENTOS RGB DE NUESTROS PIXELES
     int arrTam = 256;
     // NO ES NECESARIO UN CATEO POR QUE EN C PODEMOS ASIGNAR PUNTEROS DE TIPO VOID A UN PUNTERO DE TIPO UNSIGNED CHAR SIN NECESIDAD DE UN CASTEO EXPLICITO
@@ -117,6 +125,9 @@ void CountingSort(unsigned char A[], int width, int height, int channels) {
     int *B = (int*)calloc(arrTam, sizeof(int));
     if(channels == 1)
     {
+        double tInicialCsv = 0.0;
+        double tFinalCsv = 0.0;
+        double tInicial = omp_get_wtime();
         // CONTAMOS LA FRECUENCIA DE CADA VALOR DE A (HISTOGRAMA)
         for (int j = 0; j <= (width*height); j++) {
             C[A[j]] = C[A[j]] + 1;
@@ -131,8 +142,10 @@ void CountingSort(unsigned char A[], int width, int height, int channels) {
         }
         // YA CON LA FUNCION DE DISTRIBUCION ACUMULADA ECUALIZAMOS Y GUARDAMOS LOS VALORES
         //Ecualizacion(C, width, height);
+        tInicialCsv = omp_get_wtime();
         Ecualizacion(C, width, height);
         GuardarHistograma(B, C);
+        tFinalCsv = omp_get_wtime();
         // B ES NUESTRO HISTOGRAMA, C ES NUESTRO ECUALIZADO
         /*
         // COLOCAMOS EN B LOS NUEVOS VALORES DEL HISTOGRAMA YA EUCALIZADO 
@@ -150,13 +163,20 @@ void CountingSort(unsigned char A[], int width, int height, int channels) {
         for (int i = 0; i < (width*height); i++) {
             A[i] = (unsigned char)C[A[i]];
         }
+        double tFinal = omp_get_wtime();
+        printf("Tiempo de generacion del archivo CSV: %lfs\n", tFinalCsv-tInicialCsv);
+        // ELIMINAMOS LOS TIEMPOS DE LA GENERACION DEL ARCHIVO CSV 
+        double tTotal = (tFinal - tInicial)-(tFinalCsv- tInicialCsv);
         // LIBERAMOS LA MEMORIA 
         free(C);
         free(B);
-        return;
+        return tTotal;
     }
     else if (channels == 3)
     {   
+        double tInicialCsv = 0.0;
+        double tFinalCsv = 0.0;
+        double tInicial = omp_get_wtime();
         // OBTENDREMOS LOS VALORES DEL CANAL ROJO
         // COMO TENEMOS 3 CANALES LOS VALORES ROJOS SON LOS VALORES MULTIPLOS DE 3
         // CONTAMOS LA FRECUENCIA DE CADA VALOR DE A (HISTOGRAMA)
@@ -173,8 +193,10 @@ void CountingSort(unsigned char A[], int width, int height, int channels) {
         for (int i = 1; i < arrTam; i++) {
             C[i] = C[i] + C[i - 1];
         }
+        tInicialCsv = omp_get_wtime();
         Ecualizacion(C, width, height);
         GuardarHistograma(B, C);
+        tFinalCsv = omp_get_wtime();
         // YA CON LA FUNCION DE DISTRIBUCION ACUMULADA ECUALIZAMOS Y GUARDAMOS LOS VALORES
         //Ecualizacion(C, width, height);
         //GuardarHistograma(C, B);
@@ -197,17 +219,21 @@ void CountingSort(unsigned char A[], int width, int height, int channels) {
         for (int i = 0; i < (width*height); i++) {
             A[i * channels] = (unsigned char)C[A[i*channels]];
         }
+        double tFinal = omp_get_wtime();
+        printf("Tiempo de generacion del archivo CSV: %lfs\n", tFinalCsv-tInicialCsv);
+        // ELIMINAMOS LOS TIEMPOS DE LA GENERACION DEL ARCHIVO CSV 
+        double tTotal = (tFinal - tInicial)-(tFinalCsv- tInicialCsv);
         // LIBERAMOS LA MEMORIA 
         free(C);
         free(B);
-        return;
+        return tTotal;
     }
     else 
     {
         printf("No es posible ecualizar imagenes con ese numero de canales");
         free(C);
         free(B);
-        return;
+        return -1;
     }
     
 }   
@@ -244,3 +270,243 @@ void GuardarHistograma(int B[], int C[])
     fclose(fp); 
     return;
 }
+
+//EMPEZAMOS A DESARROLLAR LAS FUNCIONES QUE VAN A USAR EL PARALELISMO
+double splitChannelsParalel(char *Img)
+{
+    // OBTENERMOS EL VALOR QUE ALOGA A DONDE APUNTA IMG
+    char *srcPath = Img; 
+
+    int width, height, channels;
+    //EL APUNTADOR *SCRIMA APUNTA AUN ARREGLO DE TAMAÑO WIDTH*HEIGHT*CHANNELS DE LOS CANALES QUE TIENE LA IMAGEN
+    unsigned char *srcIma = stbi_load(srcPath, &width, &height, &channels, 0);
+
+    if (srcIma == NULL)
+    {
+        printf("No se pudo cargar la imagen %s :(\n\n\n", srcPath);
+        return -1;
+    }
+    else if(channels == 3)
+    {
+        printf("\nImagen cargada correctamente: %dx%d pixeles con %d canales.\n", width, height, channels);
+        int imaSize = width * height;
+
+        // SE MULTIPLICA POR EL VALOR DE CHANELS POR QUE AUNQUE SOLO VAMOS A TOMAR EN CUENTA EN CADA UNO DE LOS ARREGLOS EL VALOR CORRESPONDIENTE A CADA COLOR EN CADA PIXEL , CADA PIXEL TIENE 3 VALORES ES DECIR 3 BYTES Y DOS DE ESTOS LOS ESTABLECEREMOS EN 0 
+        unsigned char *imaBlue = malloc(imaSize * channels); 
+        unsigned char *imaRed = malloc(imaSize * channels);
+        unsigned char *imaGreen = malloc(imaSize * channels);
+        #pragma omp parallel for
+        for (int i = 0; i < imaSize; i++)
+        {
+            // CHARS ENTEROS SIN SIGNO 
+            unsigned char r = srcIma[i * channels + 0];
+            unsigned char g = srcIma[i * channels + 1];
+            unsigned char b = srcIma[i * channels + 2];
+
+            imaRed[i * channels + 0] = r;
+            imaRed[i * channels + 1] = 0;
+            imaRed[i * channels + 2] = 0;
+
+            imaGreen[i * channels + 0] = 0;
+            imaGreen[i * channels + 1] = g;
+            imaGreen[i * channels + 2] = 0;
+
+            imaBlue[i * channels + 0] = 0;
+            imaBlue[i * channels + 1] = 0;
+            imaBlue[i * channels + 2] = b;
+        }
+
+        // CON EL ARREGLO IMARED YA PODEMOS OBTENER EL HISTOGRAMA APOYANDONOS DEL ALGORITMO COUNTING SORT
+        double timeEjecParalela = CountingSortParalel(imaRed, width, height, channels);
+        // DESPUES DEL COUNTING SORT IMARED (EL ARREGLO), YA ESTA ECUALIZADO
+
+
+        // Saving image
+        stbi_write_jpg("imaBlue.jpg", width, height, 3, imaBlue, 100);
+        stbi_write_jpg("imaRed.jpg", width, height, 3, imaRed, 100);
+        stbi_write_jpg("imaGreen.jpg", width, height, 3, imaGreen, 100);
+
+        // Liberar la memoria de la imágen
+        stbi_image_free(imaBlue);  // free memory
+        stbi_image_free(imaRed);   // free memory
+        stbi_image_free(imaGreen); // free memory
+        return timeEjecParalela;
+    }
+    else if (channels == 1)
+    {
+        printf("\nImagen cargada correctamente: %dx%d pixeles con %d canales.\n", width, height, channels);
+
+        // CON EL ARREGLO IMARED YA PODEMOS OBTENER EL HISTOGRAMA APOYANDONOS DEL ALGORITMO COUNTING SORT
+        double timeEjecParalela = CountingSortParalel(srcIma, width, height, channels);
+        // DESPUES DEL COUNTING SORT IMARED (EL ARREGLO), YA ESTA ECUALIZADO
+        // Saving image
+        stbi_write_jpg("eqIma.jpg", width, height, 1, srcIma, 100);
+
+        // Liberar la memoria de la imágen
+        stbi_image_free(srcIma);  // free memory
+        return timeEjecParalela;
+    }
+    else{
+        printf("\nERROR: La imagen debe de ser de 1 0 3 canales.");
+        return -1;
+    }
+
+
+}
+double CountingSortParalel(unsigned char A[], int width, int height, int channels) {
+    // BUSQUEDA DEL MAXIMO, EN ESTE CASO NO ES NECESARIO POR QUE NUESTRSO ARREGLOS SIEMPRE SERAN DE TAMAÑO 256 POR QUE SON LOS VALORES QUE PUEDEN TOMAR LOS ELEMENTOS RGB DE NUESTROS PIXELES
+    int arrTam = 256;
+    // NO ES NECESARIO UN CATEO POR QUE EN C PODEMOS ASIGNAR PUNTEROS DE TIPO VOID A UN PUNTERO DE TIPO UNSIGNED CHAR SIN NECESIDAD DE UN CASTEO EXPLICITO
+    // USAMOS CALLOC PARA INICALIZAR EN 0 LOS ARREGLOS SIN LA NECESIDAD DE TENER QUE HACER UN FOR
+    int *C = (int*)calloc(arrTam, sizeof(int));
+    int *B = (int*)calloc(arrTam, sizeof(int));
+    if(channels == 1)
+    {
+        int num_procs = omp_get_num_procs();
+        omp_set_num_threads(num_procs);
+        printf("Numero de nucleos disponibles: %d\n", num_procs);
+        double tInicialCsv = 0.0;
+        double tFinalCsv = 0.0;
+        double tInicial = omp_get_wtime();
+    #pragma omp parallel
+    {
+        // CONTAMOS LA FRECUENCIA DE CADA VALOR DE A (HISTOGRAMA)
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        #pragma omp for 
+        for (int j = 0; j <= (width*height); j++) {
+            C[A[j]] = C[A[j]] + 1;
+        }
+
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        #pragma omp for 
+        for (int j = 0; j <= arrTam; j++) {
+            B[j] = C[j]; // IGUALAMOS B Y C PARA QUE NOS AYUDEN CON EL CSV
+        }
+        // CALCULAMOS LA FUNCION DE DISTRIBUCION ACUMULADA EN C
+        // NO ES COVENIENTE SU PARALELIZACIÓN, POR LO QUE USAMOS SINGLE, SOLO LO EJECUTA UN HILO ESTE BUCLE
+        #pragma omp single
+        {
+        for (int i = 1; i <= arrTam+1; i++) {
+            C[i] = C[i] + C[i - 1];
+        }
+        //double tFinal = omp_get_wtime();
+        //double tTotal = tFinal- tInicial;
+        // YA CON LA FUNCION DE DISTRIBUCION ACUMULADA ECUALIZAMOS Y GUARDAMOS LOS VALORES
+        //Ecualizacion(C, width, height);
+        tInicialCsv = omp_get_wtime();
+        Ecualizacion(C, width, height);
+        GuardarHistograma(B, C);
+        tFinalCsv = omp_get_wtime();
+        }
+        #pragma omp barrier
+        // B ES NUESTRO HISTOGRAMA, C ES NUESTRO ECUALIZADO
+        /*
+        // COLOCAMOS EN B LOS NUEVOS VALORES DEL HISTOGRAMA YA EUCALIZADO 
+        for (int j = arrTam; j >= 0; j--) {
+            B[C[A[j]] - 1] = (int)A[j];
+            C[A[j]] = C[A[j]] - 1;
+        }
+
+        // COPIAMOS LOS ELEMENTOS DE B -> A
+        for (int i = 0; i <= arrTam; i++) {
+            A[i] = (unsigned char)B[i];
+        }
+        */
+        // ACTUALIZAMOS EL ARREGLO A PARA QUE SE ECUALICE 
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        //tInicial = omp_get_wtime();
+        #pragma omp for
+        for (int i = 0; i < (width*height); i++) {
+            A[i] = (unsigned char)C[A[i]];
+        }
+    }
+        double tFinal = omp_get_wtime();
+        printf("Tiempo de generacion del archivo CSV: %lfs\n", tFinalCsv-tInicialCsv);
+        // ELIMINAMOS LOS TIEMPOS DE LA GENERACION DEL ARCHIVO CSV 
+        double tTotal = (tFinal - tInicial)-(tFinalCsv- tInicialCsv);
+        // LIBERAMOS LA MEMORIA 
+        free(C);
+        free(B);
+        return tTotal;
+
+    }
+    else if (channels == 3)
+    {   
+    // OBTENDREMOS LOS VALORES DEL CANAL ROJO
+        // COMO TENEMOS 3 CANALES LOS VALORES ROJOS SON LOS VALORES MULTIPLOS DE 3
+        // CONTAMOS LA FRECUENCIA DE CADA VALOR DE A (HISTOGRAMA)
+        // EL RANGO ES WIDHT*HEIGHT YA QUE COMO ESTAMOS LLENDO DE 3 EN TRES, ES COMO SI DIVIDIERAMOS EL TAMAÑO DEL ARREGLO ENTRE 3, POR ESTA RAZON NO DEBEMOS MULTIPLICAR EL RANGO POR CHANNELS 
+        int num_procs = omp_get_num_procs();
+        omp_set_num_threads(num_procs);
+        printf("Numero de nucleos disponibles: %d\n", num_procs);
+        double tInicialCsv = 0.0;
+        double tFinalCsv = 0.0;
+        double tInicial = omp_get_wtime();
+        #pragma omp parallel
+    {
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        #pragma omp for 
+        for (int j = 0; j < (width*height); j++) {
+            C[A[j * channels]] += 1;
+        }
+        // AQUI B SOLO ES UNA COPIA QUE NOS AYUDA A REPRESNETAR EL CSV, POR ESO NO MULTIPLICAMOS POR CHANNELS
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        #pragma omp for 
+        for (int j = 0; j < arrTam; j++) {
+            B[j] = C[j]; // IGUALAMOS B Y C PARA QUE NOS AYUDEN CON EL CSV
+        }
+        // CALCULAMOS LA FUNCION DE DISTRIBUCION ACUMULADA EN C
+        #pragma omp single //SOLO QUEREMOS QUE SE HAGA ESTO UNA VEZ, POR ESO EL SINGLE
+        {
+        for (int i = 1; i < arrTam; i++) {
+            C[i] = C[i] + C[i - 1];
+        }
+        //double tFinal = omp_get_wtime();
+        //double tTotal = tFinal- tInicial;
+        //B ES NUESTRO HISTOGRAMA, C ES NUESTRO ECUALIZADO
+        tInicialCsv = omp_get_wtime();
+        Ecualizacion(C, width, height);
+        GuardarHistograma(B, C);
+        tFinalCsv = omp_get_wtime();
+        }
+        // ESPERAMOS A QUE NOS ACABE DE DAR EL ECUALIZADO ESTE HILO
+        #pragma omp barrier
+        /*
+        // COLOCAMOS EN B LOS NUEVOS VALORES DEL HISTOGRAMA YA EUCALIZADO 
+        // NO HAY NECESIDAD DE CASTEO AQUI
+        for (int j = (width*height) - 1 ; j >= 0; j--) {
+            B[C[A[j * channels]] - 1] = (int)A[j * channels];
+            C[A[j * channels]] -= 1;
+        }
+
+        // COPIAMOS LOS ELEMENTOS DE B -> A TENEMOS QUE CASTEAR, NO SON EL MISMO TIPO DE DATO
+        for (int i = 0; i < (width*height); i++) {
+            A[i * channels] = (unsigned char)B[i];
+        }
+        */
+        // ACTUALIZAMOS EL ARREGLO A PARA QUE SE ECUALICE 
+        // NO HAY RIESGO DE CONDICIÓN DE CARRERA
+        //tInicial = omp_get_wtime();
+        #pragma omp for 
+        for (int i = 0; i < (width*height); i++) {
+            A[i * channels] = (unsigned char)C[A[i*channels]];
+        }
+    }
+        double tFinal = omp_get_wtime();
+        printf("Tiempo de generacion del archivo CSV: %lfs\n", tFinalCsv-tInicialCsv);
+        // ELIMINAMOS LOS TIEMPOS DE LA GENERACION DEL ARCHIVO CSV 
+        double tTotal = (tFinal - tInicial)-(tFinalCsv- tInicialCsv);
+        // LIBERAMOS LA MEMORIA 
+        free(C);
+        free(B);
+        return tTotal;
+    }
+    else 
+    {
+        printf("No es posible ecualizar imagenes con ese numero de canales");
+        free(C);
+        free(B);
+        return -1;
+    }
+    
+}   
